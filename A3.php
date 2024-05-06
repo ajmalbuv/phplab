493f5fc5fc1edb76e715376adcaadc366993cf5a
Ajmal Basheer <77728850+ajmalbuv@users.noreply.github.com>
2024-05-06 22:57:31 +0530

    maximum of three numbers

---
+++ A3.php
@@ -0,0 +1,13 @@
+<?php
+$n1 = 120;
+$n2 = 55;
+$n3 = 80;
+if ($n1 > $n2 && $n1 > $n2) {
+    echo "$n1 is maximum";
+} else
+    if ($n2 > $n1 && $n2 > $n3) {
+        echo "$n2 is maximum";
+    } else {
+        echo "$n3 is maximum";
+    }
+?>
