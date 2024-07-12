08f550eadc47ef38498b839c1321455e809a72c6
Ajmal Basheer <77728850+ajmalbuv@users.noreply.github.com>
2024-07-12 12:11:01 +0530

    database creation

---
+++ B19.php
@@ -0,0 +1,21 @@
+<?php
+// Database connection parameters 
+$servername = "localhost";
+$username = "root";
+$password = "sudo";
+$dbname = "collegedatabase";
+// Create connection 
+$conn = new mysqli($servername, $username, $password);
+// Check connection 
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+// Create database 
+$sql = "CREATE DATABASE $dbname";
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . $conn->error;
+}
+// Close connection 
+$conn->close();
