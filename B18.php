b26e8aa494709a05ab449057f6e029ac78a6b70c
Ajmal Basheer <77728850+ajmalbuv@users.noreply.github.com>
2024-07-12 12:06:39 +0530

    file Upload

---
+++ B18.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>File Upload Form</title>
+</head>
+
+<body>
+    <h1>File Upload Form</h1>
+    <form method="post" enctype="multipart/form-data">
+        <label for="file">Select a file to upload:</label>
+        <input type="file" name="file" id="file" required>
+        <input type="submit" name="submit" value="Upload File">
+    </form>
+
+    <?php
+    if (isset($_POST['submit'])) {
+        $file = $_FILES['file']['name'];
+        $fileTmpName = $_FILES['file']['tmp_name'];
+
+        // Specify the directory where the file will be uploaded 
+        $uploadDirectory = "C:\\TestFolder\\";
+
+        // Move the uploaded file to the specified directory 
+        if (move_uploaded_file($fileTmpName, $uploadDirectory . basename($file))) {
+            echo "<p>File uploaded successfully: $file</p>";
+        } else {
+            echo "<p>Failed to upload file.</p>";
+        }
+    }
+    ?>
+</body>
+
+</html>
